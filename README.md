0974ff32553b5a9f7af10965c68f047f6bf27580
Caleb Ruhm <ruhmc0619@gmail.com>
2022-04-13 20:46:55 -0400

    files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 JSFrameworks 2022 retrospective board project
 
 'npm start' to run 
-'CTRL + C' to stop
+'CTRL + C' to stop 
---
+++ src/Components/App.jsx
@@ -0,0 +1,11 @@
+import "../Styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Retro Board</h1>
+    </div>
+  );
+}
+
+export default App;
