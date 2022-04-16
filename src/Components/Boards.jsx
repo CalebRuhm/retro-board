531dfc868875c4d2d646dd3c51bb48de748235ec
Caleb Ruhm <ruhmc0619@gmail.com>
2022-04-16 17:16:29 -0400

    idk

--- src/Components/Boards.jsx
+++ src/Components/Boards.jsx
@@ -3,7 +3,22 @@ import { React, useState }from 'react'
 import { DragDropContext } from "react-beautiful-dnd"
 
 export default function Boards() {
+
+  const [title, setTitle ] = useState("Todo");
+  const [input, setInput] = useState("hidden");
+
+  const changeTitle = () => {
+    setInput("text");
+  }
+
   return (
-    <div></div>
+    <div className="Board">
+      <div className="Container">
+          <form className="Form">
+            <label onClick={changeTitle}>{title}</label>
+            <input type={input} onChange={((e) => setTitle(e.target.value))}></input>
+          </form>
+      </div>
+    </div>
   )
 }
