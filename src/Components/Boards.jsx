2a3a0fb016f5fef01fc0c9c34c351eff841e873d
CalebRuhm <ruhmc0619@gmail.com>
2022-04-25 00:49:50 -0400

    drag drop idk

--- src/Components/Boards.jsx
+++ src/Components/Boards.jsx
@@ -12,21 +12,23 @@ export default function Boards() {
     {
       title: "To do",
       newTitle: "",
-      content: [
-        "Pretend to work on ticket #6380",
-        "Shave",
-        "Get Jeff Fired",
-      ],
+      content: ["Pretend to work on ticket #6380", "Shave", "Get Jeff Fired"],
       newContent: "",
     },
     {
       title: "In Progress",
-      content: ["Bug manager to fix coffee maker!", "Develop ploy to get Jeff fired"],
+      content: [
+        "Bug manager to fix coffee maker!",
+        "Develop ploy to get Jeff fired",
+      ],
       newContent: "",
     },
     {
       title: "Finished",
-      content: ["Learn how to code haphazardly so my boss doesn't fire me and fires Jeff instead", "Eat lunch"],
+      content: [
+        "Learn how to code haphazardly so my boss doesn't fire me and fires Jeff instead",
+        "Eat lunch",
+      ],
       newContent: "",
     },
   ]);
@@ -94,60 +96,95 @@ export default function Boards() {
         <div className="container">
           {data.map((newData, idx) => {
             return (
-              <div className="Card">
-                <div className="titleContainer">
-                  <FontAwesomeIcon
-                    className="edit"
-                    icon={faPenToSquare}
-                    onClick={inputChange}
-                  />
-                  <h1 className="title">{newData.title}</h1>
-                  <FontAwesomeIcon
-                    className="trash"
-                    icon={faTrash}
-                    onClick={() => deleteCard(idx)}
-                  />
-                </div>
-                <div className="titleEdit">
-                  <input
-                    className="titleInput"
-                    type={input ? "text" : "hidden"}
-                    placeholder="Change Title"
-                    value={data[idx].newTitle}
-                    onChange={(e) => editCardTitle(idx, e)}
-                  ></input>
-                  {hidden && 
-                  <button type="submit"  onClick={() => setCardTitle(idx)}>
-                    ✓
-                  </button>
-                  }
-                </div>
-                {newData.content.map((newContent, contentIdx) => {
-                  return (
-                    <div className="row">
-                      <button onClick={() => deleteContent(idx, contentIdx)}>
-                        ✖
-                      </button>
-                      <p>{newContent}</p>
-                    </div>
-                  );
-                })}
-                <div className="add">
-                  <input
-                    type="text"
-                    placeholder="Add item"
-                    value={newData.newContent}
-                    onChange={(e) => changeContent(e, idx)}
-                  />
-                  <button
-                    type="submit"
-                    className="button"
-                    onClick={() => addContent(idx)}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
+              <DragDropContext>
+                <Droppable droppableId="data">
+                  {(provided) => {
+                    return (
+                      <div
+                        className="Card"
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                      >
+                        <div className="titleContainer">
+                          <FontAwesomeIcon
+                            className="edit"
+                            icon={faPenToSquare}
+                            onClick={inputChange}
+                          />
+                          <h1 className="title">{newData.title}</h1>
+                          <FontAwesomeIcon
+                            className="trash"
+                            icon={faTrash}
+                            onClick={() => deleteCard(idx)}
+                          />
+                        </div>
+                        <div className="titleEdit">
+                          <input
+                            className="titleInput"
+                            type={input ? "text" : "hidden"}
+                            placeholder="Change Title"
+                            value={data[idx].newTitle}
+                            onChange={(e) => editCardTitle(idx, e)}
+                          ></input>
+                          {hidden && (
+                            <button
+                              type="submit"
+                              onClick={() => setCardTitle(idx)}
+                            >
+                              ✓
+                            </button>
+                          )}
+                        </div>
+                        {newData.content.map((newContent, contentIdx) => {
+                          return (
+                            <Draggable
+                              draggableId={"content"}
+                              index={contentIdx}
+                              key={contentIdx}
+                            >
+                              {(provided) => {
+                                return (
+                                  <div
+                                    className="row"
+                                    {...provided.dragHandleProps}
+                                    {...provided.draggableProps}
+                                    ref={provided.innerRef}
+                                  >
+                                    <button
+                                      onClick={() =>
+                                        deleteContent(idx, contentIdx)
+                                      }
+                                    >
+                                      ✖
+                                    </button>
+                                    <p>{newContent}</p>
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+
+                        <div className="add">
+                          <input
+                            type="text"
+                            placeholder="Add item"
+                            value={newData.newContent}
+                            onChange={(e) => changeContent(e, idx)}
+                          />
+                          <button
+                            type="submit"
+                            className="button"
+                            onClick={() => addContent(idx)}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  }}
+                </Droppable>
+              </DragDropContext>
             );
           })}
         </div>
